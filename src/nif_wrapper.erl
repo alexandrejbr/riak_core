4434b48f1437d66266f1275f0cfaca80138117f8
Alexandre Rodrigues <alexandrejbr@live.com>
2020-05-22 13:53:50 +0200

    Working for 1 level

--- src/nif_wrapper.erl
+++ src/nif_wrapper.erl
@@ -7,17 +7,15 @@ list_to_atom(S) -> erlang:list_to_atom(S).
 binary_to_atom(S, M) -> erlang:binary_to_atom(S, M).
 
 f() ->
-  {ok, L0} = xref:q(s, "closure E || nif_wrapper"),
-  L = lists:map(fun({Caller, Callee}) -> [Caller, Callee] end, L0),
-  expand_caller(L, []).
+  {ok, L} = xref:q(s, "closure E || nif_wrapper"),
+  expand_caller(L, #{}).
 
-expand_caller(L, PreviousCallLists) ->
-  _NewCallList =
-    lists:foldl(fun([Caller | CallList], Acc) ->
-                    CallerCallers = query_function_call(Caller),
-                    append_callers(CallerCallers, Caller, Callee, Acc)
-                end,
-                PreviousCallLists, L).
+expand_caller(L, CallsMap) ->
+  lists:foldl(fun({Caller, Callee}, Acc) ->
+                  CallerCallers = query_function_call(Caller),
+                  append_callers(CallerCallers, Caller, Callee, Acc)
+              end,
+              CallsMap, L).
 
 mfa_to_list({M,F,A}) ->
   atom_to_list(M) ++ ":" ++ atom_to_list(F) ++ "/" ++ integer_to_list(A).
@@ -27,7 +25,7 @@ query_function_call(Call) ->
   L.
 
 append_callers([], Caller, EndCallee, Acc) ->
-  ;
+  Acc#{Caller => EndCallee};
 append_callers(Callers, _Callee, EndCallee, Acc) ->
   lists:foldl(fun({Caller, Callee}, FoldAcc) ->
                 FoldAcc#{[Caller, Callee] => EndCallee}
