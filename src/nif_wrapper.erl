025382838156d7e5e8e1a451cbe2c794da992ee0
Alexandre Rodrigues <alexandrejbr@live.com>
2020-05-22 15:14:55 +0200

    Failed attempt of making it recursive

--- src/nif_wrapper.erl
+++ src/nif_wrapper.erl
@@ -17,13 +17,26 @@ expand_caller(L, CallsMap) ->
                     append_callers(CallerCallers, [Caller, Callee], Acc)
                 end,
                 CallsMap, L),
-  %% maps:fold(fun(_, stop, AccIn) -> 
-  %%               AccIn;
-  %%              ([Caller | Others], continue, AccIn) ->
-  %%               CallerCallers = query_function_call(Caller),
-  %%               ok
-  %%           end, #{}, NewCallsMap).
-  NewCallsMap.
+  expand_call_map(NewCallsMap).
+
+expand_call_map(Map) ->
+  Continues = maps:filter(fun(_K, V) -> V =:= continue end, Map),
+  erlang:display(maps:size(Continues)),
+  case maps:size(Continues) > 0 of
+    true ->
+      NewMap =
+        maps:fold(fun(_, stop, AccIn) -> 
+                      AccIn;
+                     ([Caller | Others], continue, AccIn) ->
+                      CallerCallers = query_function_call(Caller),
+                      append_callers(CallerCallers, Others, AccIn)
+                  end, #{}, Map),
+      expand_call_map(NewMap);
+    _ ->
+      Map
+  end.
+  
+  
 
 mfa_to_list({M,F,A}) ->
   atom_to_list(M) ++ ":" ++ atom_to_list(F) ++ "/" ++ integer_to_list(A).
